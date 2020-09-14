64cb6026309519e60309d8f6d3543004ac834daf
root <root@1dd06423a2ed>
2020-09-14 20:26:18 +0000

    Nasty Surprise Started Module 3 completed

---
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -0,0 +1,11 @@
+package _01_nasty_surprise;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class NastySurprise {
+public static void main(String[] args) {
+	JFrame f = new JFrame();
+	JButton b = new JButton();
+}
+}
